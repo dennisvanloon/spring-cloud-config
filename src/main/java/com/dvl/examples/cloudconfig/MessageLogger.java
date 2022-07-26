3b1443cef8fca6f10cc0e3f647b1a03023721d76
Dennis van Loon <>
2022-07-26 22:21:38 +0200

    add a message logger and some config to use

---
+++ src/main/java/com/dvl/examples/cloudconfig/MessageLogger.java
@@ -0,0 +1,27 @@
+package com.dvl.examples.cloudconfig;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.concurrent.Executors.newScheduledThreadPool;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+@Component
+@Slf4j
+public class MessageLogger {
+
+    @Value("${message}")
+    private String message;
+
+    public MessageLogger() {
+        ScheduledExecutorService scheduledExecutorService = newScheduledThreadPool(1);
+        scheduledExecutorService.scheduleAtFixedRate(() -> logMessage(), 5, 5, SECONDS);
+    }
+
+    private void logMessage() {
+        log.info(message);
+    }
+}
