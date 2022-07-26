7410653e0e1b7244eab92f392066f13a2b151dcf
Dennis van Loon <>
2022-07-26 21:50:27 +0200

    initial setup

---
+++ src/main/java/com/dvl/examples/cloudconfig/Application.java
@@ -0,0 +1,16 @@
+package com.dvl.examples.cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
